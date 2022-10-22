223f386c4e45575cb7674674591e40416423575a
christian <christian.badillo@gmail.com>
2022-10-22 17:13:36 -0500

    version avance

--- asistente.py
+++ asistente.py
@@ -1,4 +1,5 @@
 from cgitb import reset
+import string
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -11,10 +12,20 @@ import os
 import webbrowser
 from googletrans import Translator
 from spanishconjugator import Conjugator
+
+from tkinter import *
+import cv2
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+import numpy as np
+
  
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voiceRate = 142
+tempFile = 'temp.jpg'
+
+
 
 voces = engine.getProperty('voices')
 engine.setProperty('voice', voces[2].id)
@@ -22,7 +33,7 @@ engine.setProperty('rate',voiceRate)
 nombre_asistente = 'pandita'
  
 def bienvenida():
-    engine.say(f"Hola me llamo {nombre_asistente}, y soy tu asistente virtual")
+    engine.say(f"Soy {nombre_asistente}, tu asistente virtual")
     engine.runAndWait()
  
 def hablar(por_hablar):
@@ -98,11 +109,10 @@ def conjuga_verbo(busca):
 def envia_whats(url):
     try:
      pywhatkit.sendwhatmsg("+525538997875", "Checate esto esta increible", datetime.datetime.now().hour, datetime.datetime.now().minute + 1)
-     print("Message Sent!") #Prints success message in console
+     print("Mensaje enviado!")
  
     except: 
-     print("Error in sending the message")
-    
+     print("Error enviando mensaje")
  
 def no_entendi():
     hablar('no te estoy entendiendo nada!')
@@ -111,8 +121,112 @@ def detener():
     hablar('Adios!')
     quit()
 
+def entrena_rostro(nombre_usuario):
+    nombre_usuario = nombre_usuario.replace(f'{nombre_asistente} entrena rostro de ','')
+    a,b = 'áéíóúü','aeiouu'
+    trans = str.maketrans(a,b)
+    nombre_usuario = nombre_usuario.translate(trans)
+    print(nombre_usuario)
+    hablar(f'Hola {nombre_usuario}, posiciona tu rostro enfrente de la cámara, y cuando estes listo presiona la tecla escape para capturarlo')
+    
+    cap = cv2.VideoCapture(0)              
+    while(True):
+        ret,frame = cap.read()              
+        cv2.imshow('Entrenando Rostro',frame)         
+        if cv2.waitKey(1) == 27:            
+            break
+    usuario_img = nombre_usuario
+    cv2.imwrite(usuario_img+".jpg",frame)       
+    cap.release()                               
+    cv2.destroyAllWindows()
+
+    def reg_rostro(img, lista_resultados):
+        data = pyplot.imread(img)
+        for i in range(len(lista_resultados)):
+            x1,y1,ancho, alto = lista_resultados[i]['box']
+            x2,y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i+1)
+            pyplot.axis('off')
+            cara_reg = data[y1:y2, x1:x2]
+            cara_reg = cv2.resize(cara_reg,(150,200), interpolation = cv2.INTER_CUBIC) 
+            cv2.imwrite(usuario_img+".jpg",cara_reg)
+            pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    img = usuario_img+".jpg"
+    pixeles = pyplot.imread(img)
+    detector = MTCNN()
+    caras = detector.detect_faces(pixeles)
+    reg_rostro(img, caras)
+    hablar(f'{nombre_usuario}, tu rostro se entrenó correctamente')
+
+def detecta_rostro():
+    hablar('Hola, posiciona tu rostro enfrente de la cámara, y cuando estes listo presiona la tecla escape para capturarlo' )
+    
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret,frame = cap.read()
+        cv2.imshow('Detectando Rostro',frame)
+        if cv2.waitKey(1) == 27:           
+            break
+    cv2.imwrite(tempFile,frame)       
+    cap.release()
+    cv2.destroyAllWindows()
+
+    def log_rostro(img, lista_resultados):
+        data = pyplot.imread(img)
+        for i in range(len(lista_resultados)):
+            x1,y1,ancho, alto = lista_resultados[i]['box']
+            x2,y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i+1)
+            pyplot.axis('off')
+            cara_reg = data[y1:y2, x1:x2]
+            cara_reg = cv2.resize(cara_reg,(150,200), interpolation = cv2.INTER_CUBIC)
+            cv2.imwrite(tempFile,cara_reg)
+            return pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    img = tempFile
+    pixeles = pyplot.imread(img)
+    detector = MTCNN()
+    caras = detector.detect_faces(pixeles)
+    log_rostro(img, caras)
+
+    im_archivos = os.listdir()  
+    detectado = False
+    for i in range(len(im_archivos)):
+        print(im_archivos[i])
+        if(im_archivos[i].__contains__('.jpg') and im_archivos[i] != tempFile):
+            print(f'validando contra {im_archivos[i]}')
+            rostro_reg = cv2.imread(im_archivos[i])
+            rostro_log = cv2.imread(tempFile)
+            similitud = orb_sim(rostro_reg, rostro_log)
+            print(similitud)
+            if similitud >= 0.95:
+                nombre_detectado = im_archivos[i].replace('.jpg','')
+                hablar(f'Ya se quien eres, eres: {nombre_detectado}')
+                detectado = True
+                break
+    if(detectado == False):
+        hablar('No se quien eres!!, Debes entrenar tu rostro previamente!')
+
+def orb_sim(img1,img2):
+    orb = cv2.ORB_create()  
+
+    kpa, descr_a = orb.detectAndCompute(img1, None)  
+    kpb, descr_b = orb.detectAndCompute(img2, None)  
+
+    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True) 
+
+    matches = comp.match(descr_a, descr_b)  
+
+    regiones_similares = [i for i in matches if i.distance < 70] 
+    if len(matches) == 0:
+        return 0
+    return len(regiones_similares)/len(matches)  
  
 bienvenida()
+
 while True:
     comando = escuchar()
  
@@ -148,5 +262,12 @@ while True:
  
         elif 'adiós' in comando:
             detener()
+        
+        elif 'entrena rostro de' in comando:
+            entrena_rostro(comando)
+
+        elif 'quién soy' in comando:
+            detecta_rostro()
+
         else:
             no_entendi()
